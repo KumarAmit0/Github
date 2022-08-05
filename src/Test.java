476856a29381657e9dd6c7bc191c7d98fb2dcd5d
Welcome <Welcome@DESKTOP-BH3FUAP>
2022-08-05 12:07:06 +0530

    First commit....

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+public class Test {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a Name");
+		String name=sc.nextLine();
+		System.out.println(name);
+	}
+
+}
